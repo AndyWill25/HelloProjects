8b5918312fa104084c005ebae9c044c6968b6494
AndyWill25 <andymac2523@gmail.com>
2021-05-13 20:38:41 -0500

    Todo app using Node.js

---
+++ NodeJS_ToDo/Client/script.js
@@ -0,0 +1,95 @@
+
+// NOTE: Original trial class code
+
+//AJAX skeleton
+// $.ajax({
+//   url: something,
+//   method: something
+// })
+// .done()
+// .fail()
+
+
+//CREATE
+$("input").keypress(function(event) {
+    if (event.which === 13 && $(this).val() !== "") {
+      var todoItem = {
+        description: $(this).val()
+      }//USE body-parser
+      $.ajax({
+        url: todoURL,
+        method: 'POST',
+        data: todoItem
+      })
+      .done(function(newToDo){
+        $('ul').append(
+          `<li data-id=${newToDo.id}>${newToDo.description}<span><i class='far fa-trash-alt'></i></span></li>`
+        );
+          $("input").val("");
+      })
+      .fail(function(errorObj){
+        console.error('Error receivng new todo.')
+      })
+
+}});
+  
+  const todoURL = 'http://localhost:3000/todos'
+
+  //READ
+  $(document).ready(function(){
+    $.ajax({
+      url: todoURL,
+      method: "GET"
+    })
+    .done(function(dataObj){
+      console.log(dataObj)
+      $('ul').empty();
+      dataObj.map(function(toDo){
+        let completed = toDo.isComplete ? "completed": "";
+        $('ul').append(
+          `<li data-id=${toDo.id} class=${completed}>${toDo.description}<span><i class='far fa-trash-alt'></i></span></li>`
+        )
+      })
+    })
+    .fail(function(errorObj){
+      console.error('Issues reading data from server.')
+    })
+  })
+
+  //UPDATE
+  $('ul').on('click', 'li', function(){
+    let self = this;
+    let thisTodoId = $(this).data('id')
+    $.ajax({
+        url: `${todoURL}/${thisTodoId}`,
+        method: 'PUT'
+    })
+    .done(function(){
+        $(self).toggleClass('completed');
+    })
+    .fail(err => console.log(`Issues with trying to update class: ${err}`))
+});
+
+  
+
+  //DELETE
+  $("ul").on("click", "span", function(event) {
+    // console.log(this)
+    event.stopPropagation()
+    let thisId = $(this).parent().data('id')
+    let self = this;
+    $.ajax({
+      url: `${todoURL}/${thisId}`,
+      method: 'DELETE'
+    })
+    .done(function(){
+    $(self).parent().remove();
+
+    })
+    .fail(function(err){
+      console.error('Issue deleting todo on backend')
+    })
+
+
+
+  });
---
+++ NodeJS_ToDo/Server/server.js
@@ -0,0 +1,97 @@
+const express = require('express')
+const app = express()
+const logger = require('morgan')
+app.use(logger(process.env.NODE_ENV))
+
+
+const cors = require('cors')
+app.use(cors())
+
+const bodyParser = require('body-parser')
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(express.static('../Client'))
+
+const PORT = process.env.PORT || 3000
+
+let toDoArr = [
+    {
+        id: 1,
+        description: 'Call mom',
+        isComplete: false,
+    },
+    {
+        id: 2,
+        description: 'Buy groceries',
+        isComplete: false,
+    },
+    {
+        id: 3,
+        description: 'Go to the movies',
+        isComplete: false,
+    }
+]
+
+app.get('/', (req, res)=>{
+    res.send('ROOOOOOOOOT')
+})
+
+//R - READ - GET Method
+app.get('/todos', (req, res)=>{
+    res.json(toDoArr)
+})
+
+
+let num = 4
+//C - CREATE - POST Method
+app.post('/todos', (req, res)=>{
+    let newToDo = 
+        {
+            id: num++,
+            description: req.body.description,
+            isComplete: false
+        }
+
+    toDoArr.push(newToDo)
+    res.status(201).json(newToDo)
+})
+
+//D - DELETE - DELETE Method
+app.delete('/todos/:id', (req, res)=>{
+    let requestedToDoID = parseInt(req.params.id)
+    let requestedToDoIndex = toDoArr.findIndex(function(todo){
+        return todo.id === requestedToDoID
+    })
+
+    if(requestedToDoIndex >= 0){
+        let deletedToDo = toDoArr.splice(requestedToDoIndex, 1)
+        res.status(223).send(deletedToDo)
+    } else {
+        res.send('ID does not exist')
+    }
+})
+
+
+//U - UPDATE - PUT Method
+app.put('/todos/:id', (req, res)=>{
+    let requestedToDoID = parseInt(req.params.id)
+    let requestedToDo = toDoArr.find(function(todo){
+        return todo.id === requestedToDoID
+    })
+
+    if(requestedToDo !== undefined){
+        requestedToDo.isComplete = !requestedToDo.isComplete
+        res.status(224).send(requestedToDo)
+    } else {
+        res.status(400).send('ID does not exist for PUT backend.')
+    }
+
+  
+})
+
+app.listen(PORT, ()=>{
+    console.log(`App listening on port ${PORT}`)
+})
