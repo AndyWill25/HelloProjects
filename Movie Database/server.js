2b2f34fc6e2313f53553558a7ce5dfec39ddf4ab
AndyWill25 <andymac2523@gmail.com>
2021-06-13 09:29:07 -0500

    Movie finder using TMDB API

--- Movie Database/server.js
+++ Movie Database/server.js
@@ -6,7 +6,6 @@ const $fetch = require('node-fetch')
 const PORT = process.env.PORT || 3000
 
 app.use(express.static('public'))
-// https://developers.themoviedb.org/3/movies/get-now-playing
 let endpoint = `https://api.themoviedb.org/3/movie/now_playing?api_key=fe806faed033a9b707afdfa3df618da0&language=en-US&page=1`
 
 
@@ -18,19 +17,10 @@ app.get('/results', (req, res)=>{
     $fetch(endpoint)
     .then(response => response.json())
     .then(data => { 
-        // arrT=[]
-        // arrP=[]
-        // data.results.forEach(el => arrT.push(el.title))
-        // data.results.forEach(el => arrP.push(el.poster_path))
-    //    console.log(arrT)
-    //    console.log(arrP)
-    //    res.render("results.ejs", {movies: arrT, arrP})
         res.render("results.ejs", {movies: data.results})
     })
     .catch()
 })
-
-
 
 app.listen(PORT, ()=>{
     console.log(`App listening on port ${PORT}`)
